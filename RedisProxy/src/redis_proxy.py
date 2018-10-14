4c20fd92491a59a34d83a5598745ecf6d62c05ae
jake17007 <jacob.beauchamp75@gmail.com>
2018-10-14 17:46:56 -0400

    ENH: added ability to pass required args on proxy startup from docker / docker-compose

--- RedisProxy/src/redis_proxy.py
+++ RedisProxy/src/redis_proxy.py
@@ -1,13 +1,19 @@
+import sys
+
 from flask import (
     Flask,
     request
 )
-from cache import Cache
-import sys
 
+from cache import Cache
+
+REDIS_ADDRESS = sys.argv[1]
+CACHE_EXP_SECS = sys.argv[2]
+CACHE_MAX_KEYS = sys.argv[3]
+PROXY_PORT = sys.argv[4]
 
 app = Flask(__name__)
-cache = Cache('redis-service', '6379', None, 10000, 100)
+cache = Cache(REDIS_ADDRESS, '6379', None, CACHE_EXP_SECS, CACHE_MAX_KEYS)
 
 
 @app.route('/', methods=['GET'])
@@ -17,4 +23,4 @@ def get():
 
 
 if __name__ == '__main__':
-    app.run(host="0.0.0.0", port=sys.argv[1], debug=True)
+    app.run(host="0.0.0.0", port=PROXY_PORT, debug=True)
